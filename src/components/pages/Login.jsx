3685fa13166618d1e4610ed11283b1e6d9ff6c47
programming-ibrahim <freelanceribrahim39@gmail.com>
2024-11-20 17:25:28 +0300

    forgot password functional add

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,11 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AuthContext/AuthProvider ';
-import { useContext, useState } from 'react';
+import { useContext, useRef, useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
 import { Helmet } from 'react-helmet-async';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../firebase.console';
 
 const Login = () => {
   const { userLogin, users, setUser } = useContext(AuthContext);
@@ -12,6 +14,7 @@ const Login = () => {
   console.log(users);
   const location = useLocation();
   console.log(location);
+
   const handleLoginSubmit = e => {
     e.preventDefault();
     const email = e.target.email.value;
@@ -25,16 +28,27 @@ const Login = () => {
       })
       .catch(error => {
         console.log('ERROR', error.message);
-        setMessErr(error.message);
 
         if (error.code === 'auth/wrong-password') {
           setMessErr('Incorrect password. Please try again.');
         } else if (error.code === 'auth/user-not-found') {
           setMessErr('No account found with this email.');
         } else {
-          setMessErr('Something went wrong. Please try again.');
+          setMessErr('Incorrect password. Please try again');
         }
       });
+  };
+  const emailRef = useRef();
+  const handleForgetPassword = () => {
+    const email = emailRef.current.value;
+    console.log('rakib', email);
+    if (!email) {
+      setMessErr('plese provide a vaild email address');
+    } else {
+      sendPasswordResetEmail(auth, email).then(() => {
+        setMessErr('please chack your email');
+      });
+    }
   };
   return (
     <>
@@ -60,7 +74,7 @@ const Login = () => {
               </label>
               <input
                 type="email"
-                id="email"
+                ref={emailRef}
                 name="email"
                 required
                 className="w-full px-4 py-2 mt-1 text-gray-900 border rounded-md border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none"
@@ -82,11 +96,12 @@ const Login = () => {
                 required
                 className="w-full px-4 py-2 mt-1 text-gray-900 border rounded-md border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none"
               />
-              {setMessErr && <p className=" text-red-600 py-2">{errMess}</p>}
+              {setMessErr && <p className=" text-red-600 pt-2">{errMess}.</p>}
 
               <label
                 htmlFor="password"
-                className="block text-sm font-medium text-gray-600 pt-2"
+                className="block text-sm font-medium text-gray-600 cursor-pointer"
+                onClick={handleForgetPassword}
               >
                 Forget Password
               </label>
